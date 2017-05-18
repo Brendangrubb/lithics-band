dc6297a9c2c60ac7f2bedc1683dbec3b2e6f856e
Brendan Grubb <brendangrubb@gmail.com>
2017-05-18 08:56:49 -0700

    add bootstrap sub theme and got it back to where is was with old theme

--- sites/all/themes/lithicsbssub/templates/page.tpl.php
+++ sites/all/themes/lithicsbssub/templates/page.tpl.php
@@ -78,7 +78,8 @@
     <div class="navbar-header">
       <?php if ($logo): ?>
         <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          <!-- GAVE MAIN-LOGO CLASS -->
+          <img class="main-logo" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
         </a>
       <?php endif; ?>
 
